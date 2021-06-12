b09b537f2c4579b7b21b9ad4850fb0e070817995
Ramandeep3 <aulakhsavi10@gmail.com>
2021-06-12 11:56:17 -0500

    update

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "./SearchBar.css"
+
+
+const SearchBar = () => {
+  return (
+    <div className="SeachBar">
+      <h1>SearchBar!</h1>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export { default as App } from './App';
-export{default as Title}from"./Title"
+export{default as Title}from"./Title";
+export { default as SearchBar } from "./SearchBar";
+export { default as CreateLinkForm } from "./CreateLinkForm"; 
+export { default as CardLink } from "./CardLink";
+export { default as EditForm } from "./EditForm"; 
