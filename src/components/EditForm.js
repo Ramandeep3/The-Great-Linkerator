936e55a421a928fca8931de47f7b8515f485b2fc
smcmanis82 <sharon.mcmanis@gmail.com>
2021-06-12 10:36:08 -0500

    added component shells

---
+++ src/components/EditForm.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+import { getSomething } from "../api";
+
+const EditForm = () => {
+  return (
+    <div className="EditForm">
+      <h1>Edit Form!</h1>
+    </div>
+  );
+};
+
+export EditForm
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as App } from "./App";
 export { default as SearchBar } from "./SearchBar";
 export { default as Title } from "./Title";
-export { default as CreateLinkForm } from "./CreateLinkForm";
+export { default as CreateLinkForm } from "./CreateLinkForm"; // sharon
 export { default as CardLink } from "./CardLink";
+export { default as EditForm } from "./EditForm"; //Jeffrey
