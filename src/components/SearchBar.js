0959924c3052dbf6a019d6792ad79b37f1c6c5b2
smcmanis82 <sharon.mcmanis@gmail.com>
2021-06-16 11:05:02 -0500

    small changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { TextField } from "@material-ui/core";
+import { TextField, Button } from "@material-ui/core";
 import { useState } from "react";
+
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="SearchBar">
+    <div className="searchbar">
       <TextField
         id="Search-Bar"
         label="Search for links"
         value={searchTerm}
         onChange={(event) => {
           setSearchTerm(event.target.value);
-          //   <button>Search</button>
+          // <button>Search</button>;
         }}
       />
     </div>
