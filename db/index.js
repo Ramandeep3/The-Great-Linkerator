fc8158d0c7cff1081c350c10cec621aa4c44f0a0
Jefferey Fitzpatrick <jeffereyfitzpatrick@Jeffereys-MBP.lan>
2021-06-15 22:23:20 -0400

    heroku updates

--- db/index.js
+++ db/index.js
@@ -1,8 +1,14 @@
 const { Client } = require("pg");
 const DB_NAME = "localhost:5432/linkerator";
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
-const client = new Client(DB_URL);
-
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || "postgres://localhost:5432/linkerator",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 // LINK database methods
 async function createLink({ name, link, count, comment, tags = [] }) {
   try {
